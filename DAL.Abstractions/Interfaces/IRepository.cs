da68d09b634c1acfe72402d38288202f68cdcc3c
Anton <aiddamaddominator@gmail.com>
2023-04-15 21:23:54 +0300

    Added UserRepository and initial UnitOfWork

---
+++ DAL.Abstractions/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.DB;
+using System.Linq.Expressions;
+
+namespace DAL.Abstractions.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    void Create(T entity);
+    Task CreateAsync(T entity);
+    T? Read(int id);
+    Task<T?> ReadAsync(int id);
+    IEnumerable<T?> ReadByCondition(Expression<Func<T, bool>> predicate);
+    Task Update(T entity);
+    void Delete(int id);
+
+}
---
+++ DAL.Abstractions/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Abstractions.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
