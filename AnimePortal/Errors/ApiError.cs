fee985b7c5b37a379bf836c3c0036b69dd4df8bd
Anton <aiddamaddominator@gmail.com>
2023-04-16 09:52:12 +0300

    Changed ExceptionMiddleware logic

---
+++ AnimePortal/Errors/ApiError.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace AnimePortalAuthServer.Errors
+{
+    public class ApiError
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string? ErrorDetails { get; set; }
+
+        public ApiError(int errorCode, string errorMessage, string? errorDetails = null)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+            ErrorDetails = errorDetails;
+        }
+
+        public override string ToString()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return JsonSerializer.Serialize(this, options);
+        }
+    }
+}
---
+++ AnimePortal/Extension/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,37 @@
+﻿using AnimePortalAuthServer.Middlewares;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace AnimePortalAuthServer.Extension
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+
+        public static void ConfigureBuiltinExceptionHandler(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(options =>
+                {
+                    options.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        var ex = context.Features.Get<IExceptionHandlerFeature>();
+                        if (ex != null)
+                        {
+                            await context.Response.WriteAsync(ex.Error.Message);
+                        }
+                    });
+                });
+            }
+        }
+    }
+}
