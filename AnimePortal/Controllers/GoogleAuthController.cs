e016a31e3e541b34f075aef3aafa2f38b084f70c
Oleksandr Turchyn <turchinsasha4@gmail.com>
2023-05-14 15:29:02 +0300

    feature: Google OAuth

---
+++ AnimePortal/Controllers/GoogleAuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimePortalAuthServer.Controllers
+{
+	public class GoogleAuthController : BaseController
+    {
+		public GoogleAuthController()
+		{
+		}
+
+        [HttpGet("google")]
+        public IActionResult GoogleLogin(string returnUrl)
+
+        {
+            var redirectUri = $"{Url.Action("GoogleCallback")}?returnUrl={returnUrl}";
+            var properties = new AuthenticationProperties { RedirectUri = redirectUri };
+           
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("google-callback")]
+        public async Task<IActionResult> GoogleCallback(string returnUrl)
+        {
+            AuthenticateResult result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (result?.Succeeded == true)
+            {
+
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
