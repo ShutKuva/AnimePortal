58efa5fc60610ce6f93f0c06c554bd416212408c
Shut Kuva <zekazyza763@gmail.com>
2023-05-18 19:21:50 +0300

    feature: add logout

--- AnimePortal/Controllers/JwtAuthController.cs
+++ AnimePortal/Controllers/JwtAuthController.cs
@@ -36,6 +36,14 @@ namespace AnimePortal.Controllers
             return Ok(result);
         }
 
+        [HttpGet("logout")]
+        public OkResult Logout()
+        {
+            Response.Cookies.Delete(CookieConstants.REFRESH_CODE_COOKIE_NAME);
+
+            return Ok();
+        } 
+
         [HttpPost("refresh")]
         public async Task<ActionResult<IUserDto>> RefreshJwtTokenAsync()
         {
