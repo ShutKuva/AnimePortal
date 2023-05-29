9e33dee333eca29f97a109574001acc389b147cf
Oleksandr Turchyn <turchinsasha4@gmail.com>
2023-05-29 22:51:23 +0300

    feat: jwt auth after google redirect

--- AnimePortal/Controllers/GoogleAuthController.cs
+++ AnimePortal/Controllers/GoogleAuthController.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using System.Security.Claims;
+using AnimePortalAuthServer.Constants;
+using Core.DB;
+using Core.DTOs.Jwt;
+using Core.DTOs.Others;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction.Interfaces;
 
 namespace AnimePortalAuthServer.Controllers
 {
-	public class GoogleAuthController : BaseController
+    public class GoogleAuthController : BaseController
     {
-		public GoogleAuthController()
+        private readonly IUserService<JwtUserDto, GoogleAuthUser, User, object> _googleAuthService;
+
+        public GoogleAuthController(IUserService<JwtUserDto, GoogleAuthUser, User, object> userService)
 		{
-		}
+            _googleAuthService = userService;
+        }
 
         [HttpGet("google")]
         public IActionResult GoogleLogin(string returnUrl)
@@ -27,16 +34,35 @@ namespace AnimePortalAuthServer.Controllers
         public async Task<IActionResult> GoogleCallback(string returnUrl)
         {
             AuthenticateResult result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-            if (result?.Succeeded == true)
+           
+            if (result == null || !result.Succeeded)
             {
+                throw new ArgumentException("Google authentication error");
+            }
 
-                return Redirect(returnUrl);
-            }
-            else
+            string name = result.Principal.Identity.Name;
+            string email = result.Principal.Claims.FirstOrDefault(asd => asd.Type == ClaimTypes.Email).Value;
+
+            var googleAuthUser = new GoogleAuthUser { Email = email, Name = name };
+
+            JwtUserDto user = await _googleAuthService.RegisterNewUserAsync(googleAuthUser);
+
+            ProcessUser(user);
+
+            return Redirect(returnUrl);
+        }
+
+        private JwtOnlyTokenDto ProcessUser(JwtUserDto user)
+        {
+            Response.Cookies.Append(CookieConstants.REFRESH_CODE_COOKIE_NAME, user.RefreshToken, new CookieOptions()
             {
-                return BadRequest();
-            }
+                HttpOnly = true,
+            });
+
+            return new JwtOnlyTokenDto
+            {
+                Token = user.Token
+            };
         }
     }
 }
--- Core/DB/User.cs
+++ Core/DB/User.cs
@@ -6,9 +6,8 @@ namespace Core.DB
     {
         public string Name { get; set; } = String.Empty;
         public string Email { get; set; } = String.Empty;
-        public string PasswordHash { get; set; } = String.Empty;
+        public string? PasswordHash { get; set; } = String.Empty;
         public string RefreshToken { get; set; } = String.Empty;
-        public DateTime RefreshTokenExpires { get; set; }
         public Roles Roles { get; set; }
     }
 }
